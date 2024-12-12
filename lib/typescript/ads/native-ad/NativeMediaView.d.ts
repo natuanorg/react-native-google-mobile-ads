d534bb9c54ef1ccfa36f5055574ce423858b4cc5
Rey [Tuan Nguyen] <tuan.nn@shopstack.asia>
2024-12-12 23:58:12 +0700

    remove lib

--- lib/typescript/ads/native-ad/NativeMediaView.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { ViewProps } from 'react-native';
-export type NativeMediaViewProps = ViewProps;
-export declare const NativeMediaView: (props: NativeMediaViewProps) => React.JSX.Element;
-//# sourceMappingURL=NativeMediaView.d.ts.map
